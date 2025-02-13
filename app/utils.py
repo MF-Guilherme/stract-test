759c6d365c77cc2e7d01b70442e61eb720cfbdfc
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2025-02-13 11:36:00 -0300

    feat(general-report): add endpoint e função para gerar o relatório geral

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,4 @@
-from app.services import get_insights_by_account_name
+from app.services import get_insights_by_account_name, search_all_platforms
 import pandas as pd
 
 
@@ -55,22 +55,50 @@ def export_collapsed_platform_report(plataforma):
     if 'id' in df.columns:
         df = df.drop(columns=['id'])
 
-    # Agrupar por account_name e platform, e somar colunas numéricas
     numeric_cols = df.select_dtypes(include='number').columns
-    df_collapsed = df.groupby(['platform','account_name']).agg({**{col: 'sum' for col in numeric_cols}, **{
+    df_collapsed = df.groupby(['platform', 'account_name']).agg({**{col: 'sum' for col in numeric_cols}, **{
         col: 'first' for col in df.columns if col not in numeric_cols and col != 'account_name' and col != 'platform'}}).reset_index()
 
-    # Deixar colunas de texto vazias, exceto account_name e platform
     for col in df_collapsed.columns:
         if col not in numeric_cols and col != 'account_name' and col != 'platform':
             df_collapsed[col] = ''
 
-    # Aplicar a função replace_dot_with_comma após o agrupamento
     df_collapsed = df_collapsed.apply(replace_dot_with_comma)
 
     df_collapsed.columns = df_collapsed.columns.str.title()
 
     csv_filename = f'{plataforma}_resumo_report.csv'
     df_collapsed.to_csv(csv_filename, sep=';', index=False)
+
+    return csv_filename
+
+
+def export_general_report():
+    platforms = ['ga4', 'tiktok_insights', 'meta_ads']
+    all_data = []
+
+    for platform in platforms:
+        insights = get_insights_by_account_name(platform)
+        for account in insights:
+            platform_name = account['platform']
+            account_name = account['account_name']
+            for insight in account['insights']:
+                insight['platform'] = platform_name
+                insight['account_name'] = account_name
+                if platform == 'ga4' and 'spend' in insight and 'clicks' in insight:
+                    insight['cost_per_click'] = insight['spend'] / insight['clicks'] if insight['clicks'] > 0 else None
+                all_data.append(insight)
+
+    df = pd.DataFrame(all_data)
+
+    if 'id' in df.columns:
+        df = df.drop(columns=['id'])
+
+    df = df.apply(replace_dot_with_comma)
+
+    df.columns = df.columns.str.title()
+
+    csv_filename = 'general_report.csv'
+    df.to_csv(csv_filename, sep=';', index=False)
 
     return csv_filename
